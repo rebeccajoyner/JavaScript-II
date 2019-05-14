eb197725bbda53682a545c021533dea5352cde1a
rebeccajoyner <49732754+rebeccajoyner@users.noreply.github.com>
2019-05-14 18:06:33 -0400

    callbacks*mult

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -43,11 +43,19 @@ last(items, last => console.log(last));
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y);
 }
+
+sumNums(2, 3, add => console.log(add));
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x * y);
+
 }
+multiplyNums(2, 3, function (number) {
+  console.log(number)
+});
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
